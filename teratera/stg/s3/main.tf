3c986b10f004ccfbe8248c7fd0b78c778d09abde
Akira Furukawa <minguu42@gmail.com>
2023-12-11 20:00:05 +0900

    一旦不要なファイルを削除する

--- teratera/stg/s3/main.tf
+++ teratera/stg/s3/main.tf
@@ -1,10 +1,10 @@
 terraform {
-  backend "s3" {
-    bucket         = "teratera-state-456"
-    key            = "global/s3/terraform.tfstate"
-    region         = "ap-northeast-1"
-    dynamodb_table = "teratera-locks"
-    encrypt        = true
+  required_version = ">= 1.0.0, < 2.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
 }
 
@@ -12,16 +12,17 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "teratera-state-456"
+resource "aws_s3_bucket" "tfstate" {
+  bucket = "teratera-stg-tfstate"
+  # 誤ってS3バケットを削除するのを防止する
   lifecycle {
-    prevent_destroy = true # 誤ってS3バケットを削除するのを防止する
+    prevent_destroy = true
   }
 }
 
 # ステートファイルの完全な履歴が見れるように、バージョニングを有効化する
 resource "aws_s3_bucket_versioning" "enabled" {
-  bucket = aws_s3_bucket.terraform_state.id
+  bucket = aws_s3_bucket.tfstate.id
   versioning_configuration {
     status = "Enabled"
   }
@@ -29,7 +30,7 @@ resource "aws_s3_bucket_versioning" "enabled" {
 
 # デフォルトでサーバサイド暗号化を有効化する
 resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
-  bucket = aws_s3_bucket.terraform_state.id
+  bucket = aws_s3_bucket.tfstate.id
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
@@ -39,19 +40,9 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
 
 # 明示的にバケットに対する全パブリックアクセスをブロックする
 resource "aws_s3_bucket_public_access_block" "public_access" {
-  bucket                  = aws_s3_bucket.terraform_state.id
+  bucket                  = aws_s3_bucket.tfstate.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-}
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "teratera-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
 }
