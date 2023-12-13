0b6af9d576f81348424c822c30433b05fcfc91fb
Akira Furukawa <minguu42@gmail.com>
2023-12-13 20:22:41 +0900

    VPC、サブネットを作成する

---
+++ teratera/stg/network/main.tf
@@ -0,0 +1,63 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket  = "teratera-stg-tfstate"
+    key     = "stg/network/terraform.tfstate"
+    region  = "ap-northeast-1"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+  tags             = {
+    Name = "teratera-stg"
+  }
+}
+
+resource "aws_subnet" "public_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.0.0/20"
+  availability_zone = "ap-northeast-1a"
+  tags              = {
+    Name = "teratera-stg-public-a"
+  }
+}
+
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.16.0/20"
+  availability_zone = "ap-northeast-1a"
+  tags              = {
+    Name = "teratera-stg-private-a"
+  }
+}
+
+resource "aws_subnet" "public_c" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.80.0/20"
+  availability_zone = "ap-northeast-1c"
+  tags              = {
+    Name = "teratera-stg-public-c"
+  }
+}
+
+resource "aws_subnet" "private_c" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.96.0/20"
+  availability_zone = "ap-northeast-1c"
+  tags              = {
+    Name = "teratera-stg-private-c"
+  }
+}
