1ed50db854c13c5039d0e486cb3d6047add10537
Akira Furukawa <minguu42@gmail.com>
2023-12-15 21:39:00 +0900

    ネットワークを作成する

--- teratera/stg/network/main.tf
+++ teratera/stg/network/main.tf
@@ -34,13 +34,6 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-  tags   = {
-    Name = "teratera-stg"
-  }
-}
-
 resource "aws_subnet" "public_a" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.0.0/20"
@@ -77,6 +70,45 @@ resource "aws_subnet" "private_c" {
   }
 }
 
+resource "aws_eip" "nat_a" {
+  domain = "vpc"
+  tags   = {
+    Name = "teratera-stg-nat-a"
+  }
+}
+
+resource "aws_eip" "nat_c" {
+  domain = "vpc"
+  tags   = {
+    Name = "teratera-stg-nat-c"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags   = {
+    Name = "teratera-stg"
+  }
+}
+
+resource "aws_nat_gateway" "a" {
+  allocation_id = aws_eip.nat_a.id
+  subnet_id     = aws_subnet.public_a.id
+  tags          = {
+    Name = "teratera-stg-a"
+  }
+  depends_on = [aws_internet_gateway.main]
+}
+
+resource "aws_nat_gateway" "c" {
+  allocation_id = aws_eip.nat_c.id
+  subnet_id     = aws_subnet.public_c.id
+  tags          = {
+    Name = "teratera-stg-c"
+  }
+  depends_on = [aws_internet_gateway.main]
+}
+
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
   route {
@@ -90,4 +122,54 @@ resource "aws_route_table" "public" {
   tags = {
     Name = "teratera-stg-public"
   }
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_c" {
+  subnet_id      = aws_subnet.public_c.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private_a" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "10.0.0.0/16"
+    gateway_id = "local"
+  }
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.a.id
+  }
+  tags = {
+    Name = "teratera-stg-private-a"
+  }
+}
+
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private_a.id
+}
+
+resource "aws_route_table" "private_c" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "10.0.0.0/16"
+    gateway_id = "local"
+  }
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.c.id
+  }
+  tags = {
+    Name = "teratera-stg-private-c"
+  }
+}
+
+resource "aws_route_table_association" "private_c" {
+  subnet_id      = aws_subnet.private_c.id
+  route_table_id = aws_route_table.private_c.id
 }
