247227c1b90dee8dab10e0aa27ea730a9536eae9
Akira Furukawa <minguu42@gmail.com>
2023-12-18 21:29:44 +0900

    SGを作成する

--- teratera/infra/terraform/vpc.tf
+++ teratera/infra/terraform/vpc.tf
@@ -91,3 +91,71 @@ resource "aws_route_table_association" "public_c" {
   subnet_id      = aws_subnet.public_c.id
   route_table_id = aws_route_table.public.id
 }
+
+# TODO: コメントアウトする
+#resource "aws_route_table" "private" {
+#  vpc_id = aws_vpc.main.id
+#  route {
+#    cidr_block = "10.0.0.0/16"
+#    gateway_id = "local"
+#  }
+#  route {
+#    cidr_block     = "0.0.0.0/0"
+#    nat_gateway_id = aws_nat_gateway.a.id
+#  }
+#  tags = {
+#    Name = "${local.product}-${var.env}-private"
+#  }
+#}
+#
+#resource "aws_route_table_association" "private_a" {
+#  subnet_id      = aws_subnet.private_a.id
+#  route_table_id = aws_route_table.private.id
+#}
+#
+#resource "aws_route_table_association" "private_c" {
+#  subnet_id      = aws_subnet.private_a.id
+#  route_table_id = aws_route_table.private.id
+#}
+
+resource "aws_security_group" "alb" {
+  name   = "${local.product}-${var.env}-alb"
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "alb_ingress" {
+  security_group_id = aws_security_group.alb.id
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_egress_rule" "alb_egress" {
+  security_group_id = aws_security_group.alb.id
+  from_port         = 0
+  to_port           = 0
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+resource "aws_security_group" "rds" {
+  name   = "${local.product}-${var.env}-rds"
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "rds_ingress" {
+  security_group_id = aws_security_group.rds.id
+  from_port         = 3306
+  to_port           = 3306
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_egress_rule" "rds_egress" {
+  security_group_id = aws_security_group.rds.id
+  from_port         = 0
+  to_port           = 0
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
+}
