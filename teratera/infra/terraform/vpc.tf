e2ee30d57bd4855b08628ff618ecb8d9025b3ff5
Akira Furukawa <minguu42@gmail.com>
2023-12-18 21:05:56 +0900

    IGWを作成する

--- teratera/infra/terraform/vpc.tf
+++ teratera/infra/terraform/vpc.tf
@@ -49,3 +49,45 @@ resource "aws_subnet" "private_c" {
 #    Name = "${local.product}-${var.env}-nat-a"
 #  }
 #}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags   = {
+    Name = "${local.product}-${var.env}"
+  }
+}
+
+# TODO: コメントアウトする
+#resource "aws_nat_gateway" "a" {
+#  allocation_id = aws_eip.nat_a.id
+#  subnet_id     = aws_subnet.public_a.id
+#  tags          = {
+#    Name = "${local.product}-${var.env}-a"
+#  }
+#  depends_on = [aws_internet_gateway.main]
+#}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "10.0.0.0/16"
+    gateway_id = "local"
+  }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+  tags = {
+    Name = "${local.product}-${var.env}-public"
+  }
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_c" {
+  subnet_id      = aws_subnet.public_c.id
+  route_table_id = aws_route_table.public.id
+}
