5bc25be9bcc1bf92ffd7c6360f97054720cbbb82
Akira Furukawa <minguu42@gmail.com>
2024-05-08 13:50:22 +0900

    schema.sqlを変更する

--- infra/mysql/schema.sql
+++ infra/mysql/schema.sql
@@ -10,9 +10,22 @@ CREATE TABLE users (
     UNIQUE INDEX (email)
 ) ENGINE = InnoDB CHARSET = utf8mb4 COLLATE = utf8mb4_bin COMMENT = 'ユーザ';
 
+CREATE TABLE projects (
+    id          CHAR(26)    NOT NULL COMMENT 'プロジェクトID',
+    user_id     CHAR(26)    NOT NULL COMMENT '所有するユーザのID',
+    name        VARCHAR(20) NOT NULL COMMENT 'プロジェクト名',
+    color       CHAR(7)     NOT NULL COMMENT 'カラー',
+    is_archived BOOLEAN     NOT NULL COMMENT 'アーカイブされたか',
+    created_at  DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '作成日',
+    updated_at  DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日',
+    PRIMARY KEY (id),
+    CONSTRAINT projects_user_id_fk FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB CHARSET = utf8mb4 COLLATE = utf8mb4_bin COMMENT = 'プロジェクト';
+
 CREATE TABLE tasks (
     id           CHAR(26)            NOT NULL COMMENT 'タスクID',
     user_id      CHAR(26)            NOT NULL COMMENT '所有するユーザのID',
+    project_id   CHAR(26)            NOT NULL COMMENT '紐づくプロジェクトのID',
     name         VARCHAR(80)         NOT NULL COMMENT 'タスク名',
     content      VARCHAR(300)        NOT NULL COMMENT 'メモ',
     priority     TINYINT(2) UNSIGNED NOT NULL CHECK (priority BETWEEN 0 AND 3) COMMENT '優先度（0~3の数字で指定し、3が最も優先度が高い）',
@@ -21,8 +34,22 @@ CREATE TABLE tasks (
     created_at   DATETIME            NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '作成日',
     updated_at   DATETIME            NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日',
     PRIMARY KEY (id),
-    CONSTRAINT tasks_user_id_fk FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT tasks_user_id_fk FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT tasks_project_id_fk FOREIGN KEY (project_id) REFERENCES projects (id) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = InnoDB CHARSET = utf8mb4 COLLATE = utf8mb4_bin COMMENT = 'タスク';
+
+CREATE TABLE steps (
+    id           CHAR(26)    NOT NULL COMMENT 'ステップID',
+    user_id      CHAR(26)    NOT NULL COMMENT '所有するユーザのID',
+    task_id      CHAR(26)    NOT NULL COMMENT '紐づくタスクのID',
+    name         VARCHAR(80) NOT NULL COMMENT 'ステップ名',
+    completed_at DATETIME COMMENT '完了日',
+    created_at   DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '作成日',
+    updated_at   DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日',
+    PRIMARY KEY (id),
+    CONSTRAINT steps_user_id_fk FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT steps_task_id_fk FOREIGN KEY (task_id) REFERENCES tasks (id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB CHARSET = utf8mb4 COLLATE = utf8mb4_bin COMMENT = 'ステップ';
 
 CREATE TABLE tags (
     id         CHAR(26)    NOT NULL COMMENT 'タグID',
